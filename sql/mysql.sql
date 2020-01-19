914503dbd075b672d6db43fef6488f682a8b3345
Hossein Azizabadi Farahani <djvoltan@gmail.com>
2020-01-19 16:42:00 +0330

    Fix database collation

--- sql/mysql.sql
+++ sql/mysql.sql
@@ -1,14 +1,14 @@
 CREATE TABLE `{token}`
 (
     `id`          INT(10) UNSIGNED    NOT NULL AUTO_INCREMENT,
-    `uid`         INT(10) UNSIGNED    NOT NULL                     DEFAULT '0',
-    `title`       VARCHAR(255)        NOT NULL                     DEFAULT '',
-    `token`       VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
-    `used`        INT(10) UNSIGNED    NOT NULL                     DEFAULT '0',
-    `time_create` INT(10) UNSIGNED    NOT NULL                     DEFAULT '0',
-    `time_used`   INT(10) UNSIGNED    NOT NULL                     DEFAULT '0',
-    `time_expire` INT(10) UNSIGNED    NOT NULL                     DEFAULT '0',
-    `status`      TINYINT(1) UNSIGNED NOT NULL                     DEFAULT '1',
+    `uid`         INT(10) UNSIGNED    NOT NULL                           DEFAULT '0',
+    `title`       VARCHAR(255)        NOT NULL                           DEFAULT '',
+    `token`       VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+    `used`        INT(10) UNSIGNED    NOT NULL                           DEFAULT '0',
+    `time_create` INT(10) UNSIGNED    NOT NULL                           DEFAULT '0',
+    `time_used`   INT(10) UNSIGNED    NOT NULL                           DEFAULT '0',
+    `time_expire` INT(10) UNSIGNED    NOT NULL                           DEFAULT '0',
+    `status`      TINYINT(1) UNSIGNED NOT NULL                           DEFAULT '1',
     PRIMARY KEY (`id`),
     UNIQUE KEY `token` (`token`),
     KEY `status` (`status`)
