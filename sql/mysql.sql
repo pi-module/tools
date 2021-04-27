3feefb535d5eacc31f3507ee1ad6c6ccc9e6715e
Hossein Azizabadi Farahani <hossein.azizabadi@gmail.com>
2021-04-27 16:19:51 +0430

    Fix errors

--- sql/mysql.sql
+++ sql/mysql.sql
@@ -18,7 +18,7 @@ CREATE TABLE `{category}`
 (
     `id`     INT(10) UNSIGNED    NOT NULL AUTO_INCREMENT,
     `title`  VARCHAR(255)        NOT NULL                           DEFAULT '',
-    `slug`   VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+    `slug`   VARCHAR(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
     `status` TINYINT(1) UNSIGNED NOT NULL                           DEFAULT '1',
     PRIMARY KEY (`id`),
     UNIQUE KEY `slug` (`slug`)
