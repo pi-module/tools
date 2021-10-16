e02d6de14e499e3cf6f29c88e54a05d5bf41f3c8
Hossein Azizabadi Farahani <hossein@azizabadi.com>
2021-10-16 15:41:32 +0330

    Add support app version

--- src/Controller/Api/VersionController.php
+++ src/Controller/Api/VersionController.php
@@ -23,29 +23,62 @@ class VersionController extends ActionController
     {
         // Get info from url
         $module = $this->params('module');
+        $platform = $this->params('platform');
+        $version = $this->params('version');
+
+        // Check
+        $platform = _escape(strtolower($platform));
+        $version = (int) $version;
 
         // Get config
         $config = Pi::service('registry')->config->read($module);
 
         // Check token
         if ($config['app_version_active']) {
+
+            // Check and set platform
+            switch ($platform) {
+                case 'android':
+                    $data = [
+                        'version'  => $config['android_version'],
+                        'url'      => $config['android_url'],
+                        'is_force' => $config['android_is_force'],
+                        'message'  => $config['android_message'],
+                    ];
+                    break;
+
+                case 'ios':
+                    $data = [
+                        'version'      => $config['ios_version'],
+                        'url'          => $config['ios_url'],
+                        'is_force'     => $config['ios_is_force'],
+                        'message'      => $config['ios_message'],
+                    ];
+                    break;
+
+                case 'pwa':
+                    $data = [
+                        'version'      => $config['pwa_version'],
+                        'url'          => $config['pwa_url'],
+                        'is_force'     => $config['pwa_is_force'],
+                        'message'      => $config['pwa_message'],
+                    ];
+                    break;
+
+                default:
+                    $data = [
+                        'version'  => '',
+                        'url'      => '',
+                        'is_force' => '',
+                        'message'  => '',
+                    ];
+                    break;
+            }
+
             // Set default result
             $result = [
                 'result' => true,
-                'data'   => [
-                    'android_version'  => $config['android_version'],
-                    'android_url'      => $config['android_url'],
-                    'android_is_force' => $config['android_is_force'],
-                    'android_message'  => $config['android_message'],
-                    'ios_version'      => $config['ios_version'],
-                    'ios_url'          => $config['ios_url'],
-                    'ios_is_force'     => $config['ios_is_force'],
-                    'ios_message'      => $config['ios_message'],
-                    'pwa_version'      => $config['pwa_version'],
-                    'pwa_url'          => $config['pwa_url'],
-                    'pwa_is_force'     => $config['pwa_is_force'],
-                    'pwa_message'      => $config['pwa_message'],
-                ],
+                'data'   => $data,
                 'error'  => [],
             ];
         } else {
